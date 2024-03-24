9990b263904f9e569672003d24801b1509847ebd
iamsdp <iamsdp1997@gmail.com>
2024-03-24 17:51:15 +0530

    CRUD operation APIs ready.

--- src/main/java/com/learnjava/Main.java
+++ src/main/java/com/learnjava/Main.java
@@ -2,9 +2,7 @@ package com.learnjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,9 +20,49 @@ public class Main {
         SpringApplication.run(Main.class, args);
     }
 
+    // GET Request to get LIST
     @GetMapping
     public List<Customer> getCustomers()
     {
         return customerRepository.findAll();
     }
+
+    // POST request to create a Customer
+    record NewCustomerReq(
+            String name,
+            String email,
+            Integer age
+    ){}
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerReq req){
+        Customer newcustomer = new Customer();
+
+        newcustomer.setName(req.name());
+        newcustomer.setEmail(req.email());
+        newcustomer.setAge(req.age());
+        customerRepository.save(newcustomer);
+    }
+
+    // Delete Customer
+    @DeleteMapping("{customerID}")
+    public void deleteCustomer(@PathVariable("customerID") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    // Update details, here we need to provide all the details else missing column will be set to its default value
+    @PutMapping("{customerID}")
+    public void updateCustomer(
+            @RequestBody NewCustomerReq req,
+            @PathVariable("customerID") Integer id)
+    {
+        Customer customer = customerRepository.findById(id).get();
+        
+        customer.setName(req.name());
+        customer.setEmail(req.email());
+        customer.setAge(req.age());
+
+        customerRepository.save(customer);
+    }
+
 }
